3813837971bdf58a335163084cb009c36c2d4ab4
Siddharth Venkatesan <neotamizhan@gmail.com>
2013-05-06 10:32:23 +0300

    added some search enhancements

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,15 @@
 var app = angular.module("app", ['ngResource']);
 
+app.factory('Poetry', function ($resource, $http) {
+    return {
 
-app.controller("PoetryCtrl", function ($scope, $resource, $http) {
+        byTerm : $resource('/api/v1/searchtext/:term'),
+        byTags : $resource('/api/v1/tags/:tags'),
+        bySerial : $resource('/api/v1/:urlkey/:serial')
+    }
+})
+
+app.controller("PoetryCtrl", function ($scope, $resource, $http, Poetry) {
 
     //$scope.cards = Cbk.creditcards.query({ cif: $scope.cif, acc: $scope.acc, civilid: $scope.civilid }, isArray = true);
 
@@ -9,13 +17,7 @@ app.controller("PoetryCtrl", function ($scope, $resource, $http) {
 
     $scope.poems = [
                     {"author":"valluvar","explanation":"As all letters have the letter A for their first, so the world has the eternal God for its first. ","name":"thirukkural","serial":1,"tags":["thirukkural","valluvar","அறத்துப்பால்","கடவுள் வாழ்த்து"],"text":["அகர முதல எழுத்தெல்லாம் ஆதி","பகவன் முதற்றே உலகு."],"translation":"A, as its first of letters, every speech maintains; <br>The \"Primal Deity\" is first through all the world's domains. ","urlkey":"kural"}
-                   ];
-
-    $scope.urlkey = "kural";                   
-
-    //$scope.searchparams = [{ name: 'CIF', value: 'cif' }, { name: 'Mobile', value: 'mobile' }, { name: 'Account', value: 'account' }, { name: 'Civil ID', value: 'civilid'}];
-
-    //$scope.criteria = $scope.searchparams[0];
+                   ];                
 
     $scope.pluralizer = {
         0: "No poems found.",
@@ -23,51 +25,40 @@ app.controller("PoetryCtrl", function ($scope, $resource, $http) {
         other: "{} poems found."
     };
 
+    /*
+        1. Is tag? then tag search
+    */
+    $scope.search = function () {
+        var term = $scope.term;
+        if (term.indexOf("tags:") > -1) {
+            // tag search
+            term = term.replace("tags:","").trim();
+            $scope.searchTags(term);
+            return;            
+        }
+
+        var searchSerial = term.match(/(\D+) (\d+)/);
+        if ( searchSerial ) {            
+            $scope.searchBySerial(searchSerial[1], searchSerial[2])
+            return;
+        }        
+
+        $scope.searchTerms();
+    }
+
+
     $scope.searchTerms = function () {
-    	console.log($scope.term);
-    	
-         $http({
-            method: 'get',
-            url: '/api/v1/searchtext/' + $scope.term
-            //params: { cif: $scope.cif, acc: $scope.acc, civilid: $scope.civilid, mobile: $scope.mobile }            
-        }).success(function (data, status) { $scope.poems = data; });
+        $scope.poems = Poetry.byTerm.query({term : $scope.term});    
     };
 
-    $scope.searchTags = function (tag) {
-         $http({
-            method: 'get',
-            url: '/api/v1/tags/' + tag
-            //params: { cif: $scope.cif, acc: $scope.acc, civilid: $scope.civilid, mobile: $scope.mobile }            
-        }).success(function (data, status) { $scope.poems = data; });        
-    }
-
-    $scope.clearSearch = function () {
-        $scope.cards = [];
-        $scope.cif = '';
-        $scope.acc = '';
-        $scope.civilid = '';
-        $scope.mobile = '';
+    $scope.searchTags = function (tags) {
+        $scope.poems = Poetry.byTags.query({tags: tags});
     };
 
-    $scope.setCriteria = function () {
-        if ($scope.searchval.length <= 7) {
-            $scope.criteria = $scope.searchparams[0];
-        }
-        else if ($scope.searchval.length == 8) {
-            $scope.criteria = $scope.searchparams[1];
-        }
-        else if ($scope.searchval.length == 10) {
-            $scope.criteria = $scope.searchparams[2];
-        }
-        else if ($scope.searchval.length == 12) {
-            $scope.criteria = $scope.searchparams[3];
-        }
-    }
+    $scope.searchBySerial = function (urlkey, serial) {
+        $scope.poems = Poetry.bySerial.query({urlkey: urlkey, serial: serial});
+    };
 
-    $scope.fmtAmt = function (amt) {
-        var symbol = amt.substr(amt.length - 1);
-        return symbol + amt.substring(0, amt.length - 1);
-    }
+    var DB = $scope.poems;
 
-    DB = $scope.cards;
 });
